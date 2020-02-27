19b9f0c02ebee6b8cfe1f0a8979f2a214e818f84
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-02-27 18:39:33 -0500

    corrected app class

--- src/main/java/com/katsidzira/ChelseaKatsidzira_URL_Shortener/ChelseaKatsidziraUrlShortenerApplication.java
+++ src/main/java/com/katsidzira/ChelseaKatsidzira_URL_Shortener/ChelseaKatsidziraUrlShortenerApplication.java
@@ -3,11 +3,14 @@ package com.katsidzira.ChelseaKatsidzira_URL_Shortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class ChelseaKatsidziraUrlShortenerApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(ChelseaKatsidziraUrlShortenerApplication.class, args);
+		run(ChelseaKatsidziraUrlShortenerApplication.class, args);
 	}
 
 }
